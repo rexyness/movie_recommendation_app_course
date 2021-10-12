6f0b5bd077e928ae27560cd78fc071d135324ab4
Aziz Hasan <abdulaziz.mhus@gmail.com>
2021-10-12 21:16:19 +0300

    removed movieflowProvider from movie_flow.dart

--- lib/features/movie_flow/movie_flow.dart
+++ lib/features/movie_flow/movie_flow.dart
@@ -3,7 +3,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:movie_recommendation_app_course/features/movie_flow/genre/genre_screen.dart';
 import 'package:movie_recommendation_app_course/features/movie_flow/landing/landing_screen.dart';
-import 'package:movie_recommendation_app_course/features/movie_flow/movie_flow_controller.dart';
 import 'package:movie_recommendation_app_course/features/movie_flow/rating/rating_screen.dart';
 import 'package:movie_recommendation_app_course/features/movie_flow/years_back/years_back_screen.dart';
 
@@ -13,7 +12,7 @@ class MovieFlow extends ConsumerWidget {
   const MovieFlow({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    ref.watch(movieFlowControllerProvider);
+    
     return PageView(
       controller: ref.watch(moviePageControllerProvider).pageController,
       physics: const NeverScrollableScrollPhysics(),
